dd4e93cd5e08e4c2cd822b76cffa256cd6552760
Ilya Zverev <zverik@textual.ru>
2017-03-08 16:03:49 +0300

    Fix deprecated functions

--- zverik/mapbbcode/acp/acp_mapbbcode_module.php
+++ zverik/mapbbcode/acp/acp_mapbbcode_module.php
@@ -21,7 +21,7 @@ class acp_mapbbcode_module
 		global $db, $user, $auth, $template;
 		global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
 
-		$action	= request_var('action', '');
+		$action	= $request->variable('action', '');
 		$submit = (isset($_POST['submit'])) ? true : false;
 
 		if ($mode != 'settings')
@@ -33,23 +33,23 @@ class acp_mapbbcode_module
 		$this->tpl_name = 'acp_mapbbcode';
 		$this->page_title = 'MAPBB_CONFIG';
 
-		$maps_enable		= request_var('maps_enable',		(bool)		$config['allow_maps']);
-		$enable_external	= request_var('enable_external',	(bool)		$config['mapbb_enable_external']);
-		$layers				= request_var('layers',				(string)	$config['mapbb_layers']);
-		$default_zoom		= request_var('default_zoom',		(int)		$config['mapbb_default_zoom']);
-		$default_pos		= request_var('default_pos',		(string)	$config['mapbb_default_pos']);
-		$view_width			= request_var('view_width',			(int)		$config['mapbb_view_width']);
-		$view_height		= request_var('view_height',		(int)		$config['mapbb_view_height']);
-		$full_height		= request_var('full_height',		(int)		$config['mapbb_full_height']);
-		$editor_height		= request_var('editor_height',		(int)		$config['mapbb_editor_height']);
-		$window_width		= request_var('window_width',		(int)		$config['mapbb_window_width']);
-		$window_height		= request_var('window_height',		(int)		$config['mapbb_window_height']);
-		$always_full		= request_var('always_full',		(bool)		$config['mapbb_always_full']);
-		$editor_window		= request_var('editor_window',		(bool)		$config['mapbb_editor_window']);
-		$standard_switcher	= request_var('standard_switcher',	(bool)		$config['mapbb_standard_switcher']);
-		$outer_link			= request_var('outer_link',			(string)	$config['mapbb_outer_link']);
-		$allowed_tags		= request_var('allowed_tags',		(string)	$config['mapbb_allowed_tags']);
-		$share_server		= request_var('share_server',		(string)	$config['mapbb_share_server']);
+		$maps_enable		= $request->variable('maps_enable',		(bool)		$config['allow_maps']);
+		$enable_external	= $request->variable('enable_external',	(bool)		$config['mapbb_enable_external']);
+		$layers				= $request->variable('layers',				(string)	$config['mapbb_layers']);
+		$default_zoom		= $request->variable('default_zoom',		(int)		$config['mapbb_default_zoom']);
+		$default_pos		= $request->variable('default_pos',		(string)	$config['mapbb_default_pos']);
+		$view_width			= $request->variable('view_width',			(int)		$config['mapbb_view_width']);
+		$view_height		= $request->variable('view_height',		(int)		$config['mapbb_view_height']);
+		$full_height		= $request->variable('full_height',		(int)		$config['mapbb_full_height']);
+		$editor_height		= $request->variable('editor_height',		(int)		$config['mapbb_editor_height']);
+		$window_width		= $request->variable('window_width',		(int)		$config['mapbb_window_width']);
+		$window_height		= $request->variable('window_height',		(int)		$config['mapbb_window_height']);
+		$always_full		= $request->variable('always_full',		(bool)		$config['mapbb_always_full']);
+		$editor_window		= $request->variable('editor_window',		(bool)		$config['mapbb_editor_window']);
+		$standard_switcher	= $request->variable('standard_switcher',	(bool)		$config['mapbb_standard_switcher']);
+		$outer_link			= $request->variable('outer_link',			(string)	$config['mapbb_outer_link']);
+		$allowed_tags		= $request->variable('allowed_tags',		(string)	$config['mapbb_allowed_tags']);
+		$share_server		= $request->variable('share_server',		(string)	$config['mapbb_share_server']);
 
 		$form_name = 'acp_mapbbcode';
 		add_form_key($form_name);
@@ -63,25 +63,25 @@ class acp_mapbbcode_module
 
 			$error = array();
 
-			set_config('allow_maps', $maps_enable);
-			set_config('mapbb_enable_external', $enable_external);
-			set_config('mapbb_layers', $layers);
-			set_config('mapbb_default_zoom', $default_zoom);
-			set_config('mapbb_default_pos', $default_pos);
-			set_config('mapbb_view_width', $view_width);
-			set_config('mapbb_view_height', $view_height);
-			set_config('mapbb_full_height', $full_height);
-			set_config('mapbb_editor_height', $editor_height);
-			set_config('mapbb_window_width', $window_width);
-			set_config('mapbb_window_height', $window_height);
-			set_config('mapbb_always_full', $always_full);
-			set_config('mapbb_editor_window', $editor_window);
-			set_config('mapbb_standard_switcher', $standard_switcher);
-			set_config('mapbb_outer_link', $outer_link);
-			set_config('mapbb_allowed_tags', $allowed_tags);
-			set_config('mapbb_share_server', $share_server);
+			$config->set('allow_maps', $maps_enable);
+			$config->set('mapbb_enable_external', $enable_external);
+			$config->set('mapbb_layers', $layers);
+			$config->set('mapbb_default_zoom', $default_zoom);
+			$config->set('mapbb_default_pos', $default_pos);
+			$config->set('mapbb_view_width', $view_width);
+			$config->set('mapbb_view_height', $view_height);
+			$config->set('mapbb_full_height', $full_height);
+			$config->set('mapbb_editor_height', $editor_height);
+			$config->set('mapbb_window_width', $window_width);
+			$config->set('mapbb_window_height', $window_height);
+			$config->set('mapbb_always_full', $always_full);
+			$config->set('mapbb_editor_window', $editor_window);
+			$config->set('mapbb_standard_switcher', $standard_switcher);
+			$config->set('mapbb_outer_link', $outer_link);
+			$config->set('mapbb_allowed_tags', $allowed_tags);
+			$config->set('mapbb_share_server', $share_server);
 
-			add_log('admin', 'LOG_CONFIG_MAPBBCODE');
+			$phpbb_log->add('admin', 'LOG_CONFIG_MAPBBCODE');
 			trigger_error($user->lang['CONFIG_UPDATED'] . adm_back_link($this->u_action));
 		}
 
